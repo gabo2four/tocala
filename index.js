9222c713e78b1c10e290d442962f1989e271ec36
Gabriel Herrera <89810766+gabo2four@users.noreply.github.com>
2023-02-23 15:36:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ import { app } from "./src/app";
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-// Conexión MongoDB
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => console.log("Conectado a MongoDB Atlas :v/"))
-  .catch((error) => console.error(error));
-
 app.listen(3001, () =>
   console.log("listening on port http://localhost:3001")
 );
