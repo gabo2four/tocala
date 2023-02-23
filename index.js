6216e2a604371bc0d56975f90b87f338667f2c19
tcmalware <genarodvo@gmail.com>
2023-02-23 15:33:25 -0500

    soy good

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import { app } from "./src/app";
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-app.listen(process.env.PORT, () =>
+// Conexión MongoDB
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Conectado a MongoDB Atlas :v/"))
+  .catch((error) => console.error(error));
+
+app.listen(3001, () =>
   console.log("listening on port http://localhost:3001")
 );
